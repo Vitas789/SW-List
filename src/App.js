924c2e7b9d64fe1c93d59ff4c1f0197641abcdef
Vitaliy <vitas.mishenko@gmail.com>
2020-12-19 16:05:01 +0700

    Оптимизация кода

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import Chars from "./Characters";
 import axios from "axios";
 
 function App() {
-  function getUrl(url) {
-    url.map((item) => {
-      axios.get(item).then((res) => {
-        item = res.data.name;
-        console.log(item);
-      });
-    });
-  }
-
   return (
     <div className="App">
-      <Chars getUrl={getUrl}></Chars>
+      <Chars />
     </div>
   );
 }
